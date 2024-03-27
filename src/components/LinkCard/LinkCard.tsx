bcfef08036fbc0e616f6339880bb04e221ca2385
Raia Quitoriano <rsquitoriano@gmail.com>
2024-03-27 10:01:59 +0800

    added variable publicUrl

--- src/components/LinkCard/LinkCard.tsx
+++ src/components/LinkCard/LinkCard.tsx
@@ -15,10 +15,13 @@ interface LinkCardProps {
 }
 
 const LinkCard: React.FC<LinkCardProps> = ({ card }) => {
+  const publicUrl = '';
+  // const publicUrl = '/public'; // for dev testing
+
   return (
     <a className="link-card" href={card.link} target="_blank">
       <div className="link-card-image">
-        <img src={`images/${card.image || "placeholder.png"}`} />
+        <img src={`${publicUrl}/images/${card.image || "placeholder.png"}`} />
       </div>
       <div className="link-card-content">
         <div className="link-card-text">
